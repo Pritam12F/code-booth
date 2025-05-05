cbf9ff58e14952bfe84189ebe19c180b7c16d3b3
Pritam Das <pritam.das.santu@gmail.com>
2025-05-05 16:17:29 +0530

    added clerk and landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/home";
 
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { NavbarSection } from "@/components/navbar";
+
+export default function HomePage() {
+  return (
+    <div className="w-screen h-screen">
+      <NavbarSection />
+    </div>
+  );
+}
